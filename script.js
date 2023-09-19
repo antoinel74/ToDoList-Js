b0dfd4070c11df5f80215c0a6b54138f236f4b2b
antoinel74 <antoine.lansman@gmail.com>
2023-09-19 11:12:59 +0200

    finish

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+const addTask = () => {
+  if (inputBox.value === "") {
+    alert("You should write smthg");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "X";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+};
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showList = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showList();
